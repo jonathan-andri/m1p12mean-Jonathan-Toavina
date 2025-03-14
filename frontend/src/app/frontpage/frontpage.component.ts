22f05119ec7c5d324f758cb0479e499a6eb391a3
Jonathan <jonathanandrinantenaina@gmail.com>
2025-03-14 16:16:11 +0300

    feat: frontpage section3-questions done

--- frontend/src/app/frontpage/frontpage.component.ts
+++ frontend/src/app/frontpage/frontpage.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { Section1Component } from "./section1/section1.component";
 import { Section2Component } from "./section2/section2.component";
+import { Section3Component } from './section3/section3.component';
 
 @Component({
   selector: 'app-frontpage',
-  imports: [HeaderComponent, Section1Component, Section2Component],
+  imports: [HeaderComponent, Section1Component, Section2Component, Section3Component],
   templateUrl: './frontpage.component.html',
   styleUrl: './frontpage.component.scss'
 })
