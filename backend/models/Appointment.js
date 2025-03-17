c458f6ac6fd1ea6661958d209e75c0f852bbe4af
Jonathan <jonathanandrinantenaina@gmail.com>
2025-03-17 16:13:13 +0300

    backend: routes appointment routes created

---
+++ backend/models/Appointment.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose') ;
+
+const appointmentSchema = new mongoose.Schema({
+    appoDesc: { type: String, required: true},
+    appoDate: { type: String, required: true},
+    appoHour: { type: String, required: true},
+    appoNote: { type: String, required: true}
+}, {timesstamps: true})
+
+module.exports = mongoose.model('Appointment', appointmentSchema);
--- frontend/src/app/customer/customer-appointment/appointment.component.ts
+++ frontend/src/app/customer/customer-appointment/appointment.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from "../../component/search-bar/search-bar.component";
+import { NewAppointmentFormComponent } from "../new-appointment-form/new-appointment-form.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-appointment',
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, NewAppointmentFormComponent, CommonModule],
   templateUrl: './appointment.component.html',
   styleUrl: './appointment.component.scss'
 })
 export class CustomerAppointmentComponent {
+  isNewAppoForm : boolean = false;
 
+  toggleAppoForm() {
+    this.isNewAppoForm = !this.isNewAppoForm;
+  }
 }
