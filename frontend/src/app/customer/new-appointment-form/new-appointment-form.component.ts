6b0bbc702eff3d9bf6874503a7f16cb2a25efdea
Jonathan <jonathanandrinantenaina@gmail.com>
2025-03-17 15:13:02 +0300

    feat: customer newAppointment form done

---
+++ frontend/src/app/customer/new-appointment-form/new-appointment-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-appointment-form',
+  imports: [],
+  templateUrl: './new-appointment-form.component.html',
+  styleUrl: './new-appointment-form.component.scss'
+})
+export class NewAppointmentFormComponent {
+
+}
