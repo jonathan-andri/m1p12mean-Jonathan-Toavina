e5f00e198bd3ab82d38f996f7efefb7e54de0989
Toavina <126314831+Herinantenaina@users.noreply.github.com>
2025-04-03 03:34:21 +0300

    Update appo-mecha.component.ts

--- frontend/src/app/component/appo-mecha/appo-mecha.component.ts
+++ frontend/src/app/component/appo-mecha/appo-mecha.component.ts
@@ -24,7 +24,6 @@ export class AppoMechaComponent implements OnInit {
     private serviceService: ServicesService,
     private notifService: NotificationService,
     private paymentService: PaymentService
-    private notifService: NotificationService
 
   ){}
   
